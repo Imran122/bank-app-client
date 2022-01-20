df7b347d7a14b8ab81cc83142abd102fb75e56d9
ghost <imransardar122@gmail.com>
2022-01-20 17:24:08 +0600

    fix 2nd

--- src/components/Withdraw/Withdraw.js
+++ src/components/Withdraw/Withdraw.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import Card from '../../context/context';
 import Footer from '../Shared/Footer/Footer';
 import Navbar from '../Shared/Navbar/Navbar';
 import './Withdraw.css'
 const Withdraw = () => {
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState('');
+    const [deposit, setDeposit] = React.useState('');
     return (
         <div>
             <Navbar></Navbar>
-            <h2>withdraw</h2>
+            <Card
+                bgcolor="primary"
+                header="Deposit"
+                status={status}
+                body={(
+
+                    <>
+                        <h4>Withdraw Amount</h4><br />
+                        <input type="input" className="form-control" id="name" placeholder="Enter nvalue" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} /><br />
+
+
+                        <button type="submit" className="btn btn-light" >Withdraw</button>
+                    </>
+                )}
+
+            />
             <Footer></Footer>
         </div>
     );
