776d38198aab4b9846732ee68028527c97e853ab
ghost <imransardar122@gmail.com>
2022-01-22 20:39:41 +0600

    fix big bug

--- src/components/CreateAccount/CreateAccount.js
+++ src/components/CreateAccount/CreateAccount.js
@@ -6,26 +6,32 @@ import { useHistory } from 'react-router-dom';
 const CreateAccount = () => {
     const [show, setShow] = useState(true);
     const [status, setStatus] = useState(false);
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+
     const [user, setUser] = useState('');
-    const ctx = React.useContext(UserContext);
-    const history = useHistory()
-    function validate(field, label) {
-        if (!field) {
-            setStatus('Error: ' + label);
-            setTimeout(() => setStatus(''), 3000);
-            return false;
-        }
-        return true;
-    }
+
+
+    /*   function validate(field, label) {
+          if (!field) {
+              setStatus('Error: ' + label);
+              setTimeout(() => setStatus(''), 3000);
+              return false;
+          }
+          return true;
+      } */
     //change handler when type in input field
     const handleOnType = e => {
         const field = e.target.name;
         const value = e.target.value;
         const newUserData = { ...user };
         newUserData[field] = value;
+        console.log(newUserData);
+        if (newUserData !== null && (newUserData.name.length > 0 || newUserData.email.length > 0 || newUserData.password.length > 0)) {
+            document.getElementById('submitbutton').disabled = false;
+        }
+        else {
+            document.getElementById('submitbutton').disabled = true;
+        }
+
         setUser(newUserData)
 
 
@@ -36,35 +42,62 @@ const CreateAccount = () => {
               if (!validate(email, 'email')) return;
               if (!validate(password, 'password')) return;
               ctx.users.push({ name, email, password, balance: 100 }); */
+
         const newUser = { ...user }
         console.log("new user", newUser);
-        console.log("submit")
-        fetch('http://localhost:5000/userList', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(newUser)
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.insertedId) {
-                    setStatus(true);
-                    setShow(false);
+        console.log("new user", newUser.email);
 
-                }
+        if (newUser.name === undefined || newUser.name === '') {
+            alert('Please enter name')
+            e.preventDefault();
+
+
+
+        }
+        else if (newUser.email === undefined || newUser.email === '') {
+            alert('Please enter email')
+            e.preventDefault();
+        }
+        else if (newUser.password === undefined || newUser.password === '') {
+            alert('please enter password')
+            e.preventDefault();
+        }
+        else if (newUser.password.length <= 8) {
+            alert('password must be 8 length')
+            e.preventDefault();
+        }
+
+        else {
+
+
+            fetch('http://localhost:5000/userList', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
             })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.insertedId) {
+
+                        setStatus(true);
+                        setShow(false);
+
+
+                    }
+                })
 
 
 
-        e.preventDefault();
+            e.preventDefault();
+        }
     }
 
-    function clearForm() {
-        setName('');
-        setEmail('');
-        setPassword('');
+    function clearForm(e) {
+        setUser('')
         setShow(true);
+
     }
     return (
         <section>
@@ -82,22 +115,22 @@ const CreateAccount = () => {
                                 </div>
                                 <form onSubmit={handleCreate}>
                                     <div className="form-field-box">
-                                        <input type="text" name="name" id="name" placeholder="Enter Name *" onBlur={handleOnType} />
+                                        <input type="text" name="name" id="name" placeholder="Enter Name *" onChange={handleOnType} />
                                         {/* Form Validation Message */}
                                         <p className="form-validation-text text-danger">Form Validation Message</p>
                                     </div>
                                     <div className="form-field-box">
-                                        <input type="email" name="email" id="email" placeholder="Enter Email *" onBlur={handleOnType} />
+                                        <input type="email" name="email" id="email" placeholder="Enter Email *" onChange={handleOnType} />
                                         {/* Form Validation Message */}
                                         <p className="form-validation-text text-danger">Form Validation Message</p>
                                     </div>
                                     <div className="form-field-box">
-                                        <input type="password" name="password" id="password" placeholder="Enter Password *" onBlur={handleOnType} />
+                                        <input type="password" name="password" id="password" placeholder="Enter Password *" onChange={handleOnType} />
                                         {/* Form Validation Message */}
                                         <p className="form-validation-text text-danger">Form Validation Message</p>
                                     </div>
                                     <div className="">
-                                        <button type="submit" className="submit-btn" >Create Account</button>
+                                        <button type="submit" id="submitbutton" className="submit-btn" disabled="disabled">Create Account</button>
                                     </div>
                                 </form>
                             </div>
