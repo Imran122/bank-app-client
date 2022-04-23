7b950b3ffdba19cf440f23ee345b09c3f8662d40
ghost <imransardar122@gmail.com>
2022-04-23 10:58:31 +0600

    table

--- src/components/AllData/AllData.js
+++ src/components/AllData/AllData.js
@@ -1,53 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../../context/context';
-import './AllData.css';
+import React, { useEffect, useState } from "react";
+import Card from "../../context/context";
+import "./AllData.css";
 const AllData = () => {
-    //set data in services
-    const [users, setUsers] = useState([])
-    //fetch data from fajedb json file
-    useEffect(() => {
-        fetch('http://localhost:5000/userListAll')
-            .then(response => response.json())
-            .then(data => setUsers(data))
-    }, [])
-    return (
-        <section>
-            <div className="container data-wrapper">
-                <Card
-                    bgcolor=""
-                    header="All Data"
-
-                    body={(
-                        <div className='table-data shadow table-responsive'>
-                            <table className="table-content rounded mb-0 table table-hover table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Email</th>
-                                        <th scope="col">Password</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {users.map(user =>
-                                        <tr key={user._id}>
-                                            <td>{user.name}</td>
-                                            <td>{user.email}</td>
-                                            <td>{user.password}</td>
-                                        </tr>
-                                    )}
-
-
-
-                                </tbody>
-                            </table>
-                        </div>
-                    )}
-                />
+  //set data in services
+  const [users, setUsers] = useState([]);
+  //fetch data from fajedb json file
+  useEffect(() => {
+    fetch("http://localhost:5000/userListAll")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <section>
+      <div className="container data-wrapper">
+        <Card
+          bgcolor=""
+          header="All Data"
+          body={
+            <div className="table-data shadow table-responsive">
+              <table className="table-content rounded mb-0 table table-hover table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Account Type</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user._id}>
+                      <td>{user.displayName}</td>
+                      <td>{user.email}</td>
+                      <td>{user.accountType}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-        </section>
-    );
+          }
+        />
+      </div>
+    </section>
+  );
 };
 
 export default AllData;
-
-
